3437356105405bea9e2712203f0f767bd31f21c3
lvazquezdorna <lvazquezdorna@nautilus17.danielcastelao.org>
2018-11-28 12:07:18 +0100

    1.Empezado

---
+++ Boletin12_1.Garaje/src/boletin12_1/garaje/Boletin12_1Garaje.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package boletin12_1.garaje;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class Boletin12_1Garaje {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ Boletin12_1.Garaje/src/boletin12_1/garaje/Garaje.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin12_1.garaje;
+
+/**
+ *
+ * @author Lara Vazquez Dorna
+ */
+public class Garaje {
+    
+    //Atributos
+    int numCoches;
+    String matricula;
+   
+    
+    public void comprobarSitio(){
+    
+    }
+    
+    public void entradaCoche(){
+    
+    }
+    public void calcularPrecio(){
+    
+    }
+    
+    public void imprimirFactura(){
+    
+    }
+}
